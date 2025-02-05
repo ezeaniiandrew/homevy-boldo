1e382d59e850bc9d3b221a6071c77c58e8a1c1d6
ezeaniiandrew <ezeaniiandrew@gmail.com>
2025-02-05 19:42:56 +0100

    move hero section to a component

---
+++ src/components/Hero.tsx
@@ -0,0 +1,42 @@
+import CompanySlideshow from "@/components/CompanySlideshow";
+import Image from "next/image";
+import Link from "next/link";
+
+export default function Hero() {
+  return (
+    <section className="px-[6.25rem] mb-[5rem] bg-dark-blue col-span-full pt-40">
+      <div className="flex justify-between items-center">
+        <div className="max-w-[35rem]">
+          <h1 className="font-manrope text-5xl font-normal leading-[4.5rem]">
+            Save time by building fast with Boldo Template
+          </h1>
+          <p className="text-light-grey leading-[1.6rem] mt-4">
+            Funding handshake buyer business-to-business metrics iPad
+            partnership. First mover advantage innovator success deployment
+            non-disclosure.
+          </p>
+          <div className="mt-10 flex gap-6 capitalize">
+            <Link
+              href={"#"}
+              className="px-14 py-4 bg-green border-2 border-green rounded-[56px] text-xl font-bold text-dark-blue"
+            >
+              Buy template
+            </Link>
+            <Link
+              href={"#"}
+              className="px-14 py-4 border-2 border-white rounded-[56px] text-xl font-bold"
+            >
+              explore
+            </Link>
+          </div>
+        </div>
+        <div>
+          <Image src="/hero-graphics.svg" alt="hero" width={494} height={423} />
+        </div>
+      </div>
+      <div className="mt-[3.5rem] mb-[3.5rem]">
+        <CompanySlideshow />
+      </div>
+    </section>
+  );
+}
